a5e3debbc22695238baed855b259e9af7e6eb2f6
NoahL <lauffern@gmail.com>
2022-03-23 08:58:57 -0300

    INIT PUSH

---
+++ src/components/DayListItem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import 'components/DayListItem.scss'
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+
+  let dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0}) 
+
+
+
+  const formatSpots = function(spot) {
+    if (spot === 0)  {
+
+      return spot = 'no spots remaining'
+
+    } else if (spot === 1) {
+
+      return spot = '1 spot remaining'
+
+    } else {
+
+      return spot  + ' spots remaining'
+
+    }
+  };
+    
+  let newSpots = formatSpots(props.spots)
+
+
+  return (
+    <li
+    className={dayClass} 
+    onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{newSpots}</h3>
+    </li>
+  );
+}
